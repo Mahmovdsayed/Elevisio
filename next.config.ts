3815daa0455a8c6c2eabc6184583e30523da0a3b
Mahmovdsayed <mahmoudsayed3576@gmail.com>
2025-03-12 11:37:39 +0200

    rs

--- next.config.ts
+++ next.config.ts
@@ -1,6 +1,17 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
+  reactStrictMode: true,
+  images: {
+    domains: ["res.cloudinary.com"],
+  },
+  productionBrowserSourceMaps: true,
+  swcMinify: true,
+
+  compiler: {
+    removeConsole: process.env.NODE_ENV === "production",
+  },
+
   experimental: {
     serverActions: {
       bodySizeLimit: "10mb",
